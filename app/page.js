d32773e9e1e5cc0577104018ef5f6cfc89fd909c
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2024-01-29 10:10:23 -0700

    added nav bar to home

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import NavBar from "./nav/nav-bar";
 
 export default function Page() {
   return (
     <main> 
+        <NavBar />
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
         <ul>
           <li><Link href="week-2">Week-2</Link></li>
