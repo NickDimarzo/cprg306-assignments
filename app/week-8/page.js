516c7bcb5f1cec5c08db881bb60cd63c73b1bf2e
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2024-03-13 12:18:48 -0600

    tailwind

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 
-
 import { useUserAuth } from "./_utils/auth-context";
 import NavBar from "../nav/nav-bar";
 
@@ -9,28 +8,55 @@ export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
 
   return (
-  
     <div>
       <header>
         <NavBar />
       </header>
-        {user ? 
-        (
-          <div>
-            <h1>Week 8</h1>
-            <p>Hi there! {user.displayName}</p>
-            <Link href="/week-8/shopping-list">Shopping List</Link>
-            <button onClick={firebaseSignOut}>Sign Out</button>
+      <div className="p-2 m-4 flex justify-center">
+        {user ? (
+          <div className="max-w-sm p-2 m-4 bg-slate-900 w-full border-2 rounded-lg text-white font-bold text-3xl flex-col justify-center">
+            <div className="flex justify-center py-4">
+              <h1>Week 8</h1>
+            </div>
+            <div className="flex justify-center py-4">
+              <p>Hi there! {user.displayName}</p>
+            </div>
+            <div className="flex justify-center py-4">
+              <Link
+                href="/week-8/shopping-list"
+                className="bg-slate-400 px-8 py-2 rounded-lg text-white border-2 border-slate-400 hover:border-green-500 hover:text-green-500 m-1 font-bold w-full text-center"
+              >
+                Shopping List
+              </Link>
+            </div>
+            <div className="flex justify-center pt-4">
+              <button
+                onClick={firebaseSignOut}
+                className="bg-slate-400 px-8 py-2 rounded-lg text-white border-2 border-slate-400 hover:border-green-500 hover:text-green-500 m-1 font-bold w-full"
+              >
+                Sign Out
+              </button>
+            </div>
           </div>
-          
-        ) : 
-        (
-          <div>
-            <h1>Week 8</h1>
-            <p>Please sign in</p>
-            <button onClick={gitHubSignIn}>Sign In with GitHub</button>
+        ) : (
+          <div className="max-w-sm p-2 m-4 bg-slate-900 w-full border-2 rounded-lg text-white font-bold text-3xl flex-col justify-center">
+            <div className="flex justify-center py-4">
+              <h1>Week 8</h1>
+            </div>
+            <div className="flex justify-center py-4">
+              <p>Please sign in</p>
+            </div>
+            <div className="flex justify-center pt-4">
+              <button
+                onClick={gitHubSignIn}
+                className="bg-slate-400 px-8 py-2 rounded-lg text-white border-2 border-slate-400 hover:border-green-500 hover:text-green-500 m-1 font-bold w-full"
+              >
+                Sign In with GitHub
+              </button>
+            </div>
           </div>
-        )}    
+        )}
+      </div>
     </div>
   );
 }
